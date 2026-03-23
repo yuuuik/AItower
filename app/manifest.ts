8218d65f66b6d20b914f771aac1538ed9f243c0b
yuuuik <fxserzh@gmail.com>
2026-03-23 19:32:14 +0100

    Fix manifest icon purpose type error

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/towerrush.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
     categories: ['games', 'entertainment'],
