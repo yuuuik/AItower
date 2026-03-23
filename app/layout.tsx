1880ad912c3ca2b5f3a073cf7491cf49849068a8
yuuuik <fxserzh@gmail.com>
2026-03-23 21:34:28 +0100

    Add Yandex Metrika counter to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { SITE_URL } from '@/lib/constants';
 
@@ -89,6 +90,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
+        <Script id="yandex-metrika" strategy="afterInteractive">{`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108208154', 'ym');
+          ym(108208154, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}</Script>
+        <noscript>
+          <div><img src="https://mc.yandex.ru/watch/108208154" style={{position:'absolute', left:'-9999px'}} alt="" /></div>
+        </noscript>
         {children}
       </body>
     </html>
