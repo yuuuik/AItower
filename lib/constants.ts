83834bf1ee5ed514ce8959eaa599d81c0b1b7435
yuuuik <127669989+yuuuik@users.noreply.github.com>
2026-04-02 16:47:41 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const REAL_MONEY_URL = 'https://one-vv7312.life/?open=register&p=n7vz';
+export const REAL_MONEY_URL = 'https://one-vv4905.com/casino/list?open=register&p=q4lj';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://towerrush-predictor.vercel.app';
