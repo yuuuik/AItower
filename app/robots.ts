2c345cfd327bca4fdcebafacac83b0d45958c5ef
yuuuik <fxserzh@gmail.com>
2026-07-28 15:46:06 +0400

    Fix: убрать двойной слэш в sitemap URL внутри robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/'],
       },
     ],
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: `${SITE_URL.replace(/\/$/, '')}/sitemap.xml`,
   };
 }
